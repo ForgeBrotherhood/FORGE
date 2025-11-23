480ab6d79179564617e5b4d900b6720ac89e7c5d
ForgeBrotherhood <austin.dearing@yahoo.com>
2025-11-23 15:19:49 -0800

    Update metro.config.js

---
+++ mobile/metro.config.js
@@ -0,0 +1,9 @@
+// metro.config.js
+// Metro configuration for the FORGE Expo app.
+
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+module.exports = config;
