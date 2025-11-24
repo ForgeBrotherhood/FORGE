4751094a2147fceb80cfd4f12eae0bd4a7bc4465
ForgeBrotherhood <austin.dearing@yahoo.com>
2025-11-24 15:01:34 -0800

    Update App.tsx

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,28 +1,7 @@
-// mobile/src/App.tsx
+// mobile/App.tsx
 import React from "react";
-import { StatusBar } from "react-native";
-import { NavigationContainer, DefaultTheme, Theme } from "@react-navigation/native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import RootNavigation from "./src/navigation";
 
-import RootNavigator from "./navigation";
-
-const navTheme: Theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    background: "#050608" // FORGE dark background
-  }
-};
-
-const App: React.FC = () => {
-  return (
-    <SafeAreaProvider>
-      <NavigationContainer theme={navTheme}>
-        <StatusBar barStyle="light-content" />
-        <RootNavigator />
-      </NavigationContainer>
-    </SafeAreaProvider>
-  );
-};
-
-export default App;
+export default function App() {
+  return <RootNavigation />;
+}
